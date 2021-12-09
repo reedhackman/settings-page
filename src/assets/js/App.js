04b925f9b254e8db9405c3e51741df88776ac643
reedhackman <42731645+reedhackman@users.noreply.github.com>
2021-12-09 11:47:22 -0800

    Update App.js

--- src/assets/js/App.js
+++ src/assets/js/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Settings/>
+      <script defer src="https://connect.podium.com/widget.js#API_TOKEN=66d440e6-5d37-48af-93bc-1b4acacea8cc" id="podium-widget" data-api-token="66d440e6-5d37-48af-93bc-1b4acacea8cc"></script>
     </div>
   );
 }
